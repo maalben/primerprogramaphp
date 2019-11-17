32405dd793604d0965a469a0517ea026cd7e34a2
Mario Alejandro Benítez Orozco <maalben@gmail.com>
2019-11-17 12:53:06 -0500

    Se agrega hoja de estilo css

---
+++ biografia_don_ramon.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Biografía de Don Ramón</title>
+    <link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body>
+        <h1>Biografía de Don Ramón</h1>
+        <p>
+        Ramón Antonio Esteban Gómez de Valdés y Castillo (Ciudad de México, 2 de septiembre de 1923-Ib., 9 de agosto de 1988), más conocido como Ramón Valdés, fue un actor de cine y televisión mexicano conocido por interpretar a Don Ramón en El Chavo del 8, aunque también por sus múltiples papeles de comedia en diferentes programas.
+        </p>
+        <p>
+        Nacido en Ciudad de México, se crió en una familia humilde y numerosa que más tarde se mudaría a Ciudad Juárez cuando tenía dos años. Valdés hizo su debut como actor en el cine en la película Calabacitas tiernas (1949) apareciendo junto con su hermano mayor, el actor Germán Valdés, quien fue el que lo introdujo en el mundo de la actuación. Bajo papeles de extra o roles secundarios, continuó haciendo apariciones en películas durante la época de Oro del cine mexicano. Ramón y Germán tenían otros dos hermanos también actores Manuel y Antonio Valdés.
+        </p>
+        <p>        
+        En 1968, conoció a Roberto Gómez Bolaños, con quien comenzó a trabajar en múltiples programas como Los supergenios de la mesa cuadrada, Chespirito y El Chapulín Colorado pero fue en la comedia popular de Bolaños El Chavo del 8 con la que ganó fama internacional por su interpretación de Don Ramón en la serie. Dejó el programa de El Chavo del Ocho en 1979, pero regresó a él en 1981, siendo este su último año en el proyecto. En 1982, Valdés protagonizó con Carlos Villagrán en el programa venezolano Federrico y en ¡Ah, qué Kiko! en 1987, aunque ambos programas no tuvieron el éxito que se esperaba.
+        </p>
+        <img id="imagen2" src="imagenes/440px-Ramón_Valdés_Don_Ramón_(cropped).jpg">
+        <br>
+        <a href="index.php">Volver</a>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>
+    Hola Mundo !
+</title>
+<link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body>
+    <!-- Esto es un comentario  -->
+    <h1>Hola mundo, me llamo Anacleto!</h1>
+    <h2>Subtítulo</h2>
+    <h3>Contenido de información</h3>
+    <h4>Un texto pequeño</h4>
+    <h5>Un texto más pequeño</h5>
+    <h6>Muy pequeño</h6>
+    <p>Esto es un párrafo para colocar mucha información</p>
+    <p>Tengo un texto con estilo <b>Negrita</b></p>
+    <a href="http://www.google.com">Clic aquí para ir a google</a>
+    <br>
+    Hola mundo 2.
+    <br>
+    Este es otro texto.
+    <br>
+    <a href="biografia_don_ramon.php"><img id="imagen1" src="imagenes/donramon.jpg"></a>
+</body>
+</html>
